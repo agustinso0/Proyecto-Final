c98274ff6e41dcf2ced189762946fefc545eafa4
Agustin Loos <loosagustin@gmail.com>
2025-06-21 13:26:53 -0300

    feat: Agregar modelo de tipo de transacción

---
+++ backend/models/TransactionTypes.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const TransactionTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("TransactionType", TransactionTypeSchema);
