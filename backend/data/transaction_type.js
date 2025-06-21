f58fbe491e0d2a92b60fcdc1ae717fc33641722b
Agustin Loos <loosagustin@gmail.com>
2025-06-21 13:26:23 -0300

    feat: Agregar archivo de tipos de transacción con datos iniciales

---
+++ backend/data/transaction_type.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+module.exports = [
+  {
+    _id: "6856d6b53a7da566e87bf3f0",
+    name: "Egreso",
+  },
+  {
+    _id: "6856d6b53a7da566e87bf3f1",
+    name: "Ingreso",
+  },
+];
