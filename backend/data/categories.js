a28958bbee2195ff04fe1d881087877c2ed55d21
Agustin Loos <loosagustin@gmail.com>
2025-06-21 13:26:21 -0300

    feat: Agregar archivo de categorías con datos iniciales

---
+++ backend/data/categories.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+module.exports = [
+  {
+    _id: "6856d7ebe9b3da3353bbccdf",
+    name: "Tecnologia",
+  },
+  {
+    _id: "6856d7ebe9b3da3353bbcce0",
+    name: "Comida",
+  },
+  {
+    _id: "6856d7ebe9b3da3353bbcce1",
+    name: "Hogar",
+  },
+  {
+    _id: "6856d7ebe9b3da3353bbcce2",
+    name: "Moda",
+  },
+  {
+    _id: "6856d7ebe9b3da3353bbcce3",
+    name: "Animales",
+  },
+];
