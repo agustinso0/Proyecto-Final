0cecaec8ed7d58d079830f6471cf89f3c73e54c4
Agustin Loos <loosagustin@gmail.com>
2025-06-21 12:35:38 -0300

    feat: Agregar datos de ejemplo para usuarios en el módulo de usuarios

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    nombre: "Juan",
+    apellido: "Pérez",
+    email: "juan@example.com",
+    direccion: "Calle Falsa 123",
+    saldo: 1000,
+  },
+  {
+    nombre: "María",
+    apellido: "Gómez",
+    email: "maria@example.com",
+    direccion: "Av. Siempre Viva 742",
+    saldo: 1500,
+  },
+  {
+    nombre: "Agustin",
+    apellido: "Ferraro",
+    email: "agustin@example.com",
+    direccion: "Av. Super Falsa 666",
+    saldo: 3500,
+  },
+];
