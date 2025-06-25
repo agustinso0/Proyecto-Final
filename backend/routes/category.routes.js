3884a79316383c2bc281a7f7af4245a59be9b091
Mateo Muscolino <muscolinomateo@gmail.com>
2025-06-25 10:45:52 -0300

    feat: Add authentication middleware to category and transaction routes

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const {getAllCategories, deleteCategoryByName, createCategory} = require("../controllers/category.controller");
-
+const { authenticate } = require("../middleware/auth")
 
 const router = express.Router();
 
+//Ruta publica
 router.get("/", getAllCategories);
+
+
+router.use(authenticate);
+
+//Rutas protegidas
 router.delete("/:name", deleteCategoryByName);
 router.post("/", createCategory);
 
--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const {getAllTransactions} = require("../controllers/transaction.controller");
-
+const {authenticate} = require("../middleware/auth");
 const router = express.Router();
 
+
+router.use(authenticate);
+//Ruta protegida
 router.get("/", getAllTransactions);
 
 module.exports = router;
