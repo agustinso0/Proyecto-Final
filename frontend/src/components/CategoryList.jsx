04b61ca25cd41f04d6b49967dcaf3c21fbcf0927
Agustin Loos <loosagustin@gmail.com>
2025-07-13 16:50:22 -0300

    refactor: unificar comillas en CategoryList.jsx para mejorar la consistencia del estilo

--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -1,59 +1,51 @@
-import { useEffect, useState } from 'react'
-import {obtenerCategorias, crearCategoria} from '../services/category.service'
+import { useEffect, useState } from "react";
+import { createCategory, getAllCategories } from "../services/category.service";
 
-export function CategoryList(){
-    const [categories, setCategories] = useState([]);
-    const [newCategory, setNewCategory] = useState('');
-    
+export default function CategoryList() {
+  const [categories, setCategories] = useState([]);
+  const [newCategory, setNewCategory] = useState("");
 
-    useEffect(()=> {
-        async function fetchCategorias() {
-            try {
-                const data = await obtenerCategorias();
-                setCategories(data.data)
-            } catch (error) {
-                console.error("Error al obtener las categorias:", error)
-            }
-        }
-        fetchCategorias();
-    },[])
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault(); // evita el refresh
-        if(!newCategory.trim()) return;
-        try {
-            await crearCategoria(newCategory);
-            const data = await obtenerCategorias();
-            setCategories(data.data);
-            setNewCategory('')
-        } catch (error) {
-            console.error("Error al crear la categoria:", error);
-        }
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const data = await getAllCategories();
+        setCategories(data.data);
+      } catch (error) {
+        console.error("Error al obtener las categorias:", error);
+      }
     }
+    fetchCategories();
+  }, []);
 
-    return(
-        <div>
-            
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="text" 
-                value={newCategory} 
-                onChange={e => setNewCategory(e.target.value)}
-                placeholder='Nueva Categoria' 
-                />
-                <button type='submit'>Crear</button>
-            </form>
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // evita el refresh
+    if (!newCategory.trim()) return;
+    try {
+      await createCategory(newCategory);
+      const data = await getAllCategories();
+      setCategories(data.data);
+      setNewCategory("");
+    } catch (error) {
+      console.error("Error al crear la categoria:", error);
+    }
+  };
 
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newCategory}
+          onChange={(e) => setNewCategory(e.target.value)}
+          placeholder="Nueva Categoria"
+        />
+        <button type="submit">Crear</button>
+      </form>
 
-
-            <ul>
-                {categories && categories.map(cat =>(
-                    <li key={cat._id}>{cat.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
-    
+      <ul>
+        {categories &&
+          categories.map((cat) => <li key={cat._id}>{cat.name}</li>)}
+      </ul>
+    </div>
+  );
 }
-
