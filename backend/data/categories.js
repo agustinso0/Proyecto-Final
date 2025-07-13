1ef63a06f23ab0a1edc335d1122e1c150386e479
Agustin Loos <loosagustin@gmail.com>
2025-07-13 18:20:06 -0300

    feat: agregar propiedad isActive a todas las categorías

--- backend/data/categories.js
+++ backend/data/categories.js
@@ -4,21 +4,26 @@ module.exports = [
   {
     _id: "6856d7ebe9b3da3353bbccdf",
     name: "Tecnologia",
+    isActive: true,
   },
   {
     _id: "6856d7ebe9b3da3353bbcce0",
     name: "Comida",
+    isActive: true,
   },
   {
     _id: "6856d7ebe9b3da3353bbcce1",
     name: "Hogar",
+    isActive: true,
   },
   {
     _id: "6856d7ebe9b3da3353bbcce2",
     name: "Moda",
+    isActive: true,
   },
   {
     _id: "6856d7ebe9b3da3353bbcce3",
     name: "Animales",
+    isActive: true,
   },
 ];
