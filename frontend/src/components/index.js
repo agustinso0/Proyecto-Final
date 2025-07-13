06e0c7bead5b5ef1e783e24d703f9ea768e97a50
Agustin Loos <loosagustin@gmail.com>
2025-07-13 16:50:32 -0300

    feat: agregar archivo index.js para exportar componentes de usuario, transacción y categoría

---
+++ frontend/src/components/index.js
@@ -0,0 +1,13 @@
+export { default as UserList } from "./users/UserList";
+export { default as EditUserForm } from "./users/EditUserForm";
+export { default as UserCard } from "./users/UserCard";
+
+export { default as TransactionList } from "./TransactionList";
+export { default as TransactionForm } from "./TransactionForm";
+
+export { default as CategoryForm } from "./CategoryForm";
+export { default as CategoryList } from "./CategoryList";
+
+export { default as BalanceCard } from "./BalanceCard";
+
+export { LoginForm, RegisterForm } from "./auth";
