0078c4694ce0e13b80b90f4fd640b16cdc372403
Agustin Loos <loosagustin@gmail.com>
2025-07-13 18:18:43 -0300

    feat: mejorar controladores de categorías con respuestas estandarizadas

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,17 +1,52 @@
 const CategoryService = require("../services/category.service");
+const ApiResponse = require("../utils/ApiResponse");
 
 const getAllCategories = async (req, res) => {
-    const categories = await CategoryService.getAllCategories();
-    res.json({ data: categories });
+  const result = await CategoryService.getAllCategories();
+  res
+    .status(200)
+    .json(new ApiResponse(200, result, "Categorías obtenidas exitosamente"));
+};
+
+const getCategory = async (req, res) => {
+  const category = await CategoryService.getCategoryById(req.params.id);
+  res.status(200).json(new ApiResponse(200, category, "Categoría encontrada"));
 };
 
 const createCategory = async (req, res) => {
-    const { name } = req.body;
-    const category = await CategoryService.createCategory(name);
-    res.json({ data: category });
+  const newCategory = await CategoryService.createCategory(req.body);
+  res
+    .status(201)
+    .json(new ApiResponse(201, newCategory, "Categoría creada exitosamente"));
+};
+
+const updateCategory = async (req, res) => {
+  const updatedCategory = await CategoryService.updateCategory(
+    req.params.id,
+    req.body
+  );
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedCategory,
+        "Categoría actualizada exitosamente"
+      )
+    );
+};
+
+const deleteCategory = async (req, res) => {
+  const result = await CategoryService.deleteCategory(req.params.id);
+  res
+    .status(200)
+    .json(new ApiResponse(200, result, "Categoría eliminada exitosamente"));
 };
 
 module.exports = {
-    getAllCategories,
-    createCategory
+  getAllCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
 };
