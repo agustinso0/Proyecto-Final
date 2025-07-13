1a805c08549eb308c97135ac4997654c62d02edf
Agustin Loos <loosagustin@gmail.com>
2025-07-13 16:52:36 -0300

    feat: agregar hooks de usuario y autenticación

---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,16 @@
+export {
+  useUsers,
+  useUserById,
+  useUserByEmail,
+  useUserMutations,
+} from "./useUsers";
+
+export { useUserHandlers } from "./useUserHandlers";
+
+export {
+  useAuth,
+  useAuthData,
+  useAuthMutations,
+  useCurrentUser,
+  useSessions,
+} from "./useAuth";
