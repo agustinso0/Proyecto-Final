95337eed7a14428f7fadead61c7da23a43b623a7
Agustin Loos <loosagustin@gmail.com>
2025-07-13 18:20:23 -0300

    feat: agregar métodos para obtener, actualizar y eliminar categorías en las rutas

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,10 +1,33 @@
 const express = require("express");
-const { getAllCategories, createCategory } = require("../controllers/category.controller");
+const {
+  getAllCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/category.controller");
 const { authenticate } = require("../middleware/auth");
+const validateRequest = require("../middleware/validateRequest");
+const {
+  categoryValidationRules,
+  categoryUpdateValidationRules,
+  categoryIdValidation,
+} = require("../validators/category.validator");
+
 const router = express.Router();
 
 router.use(authenticate);
+
 router.get("/", getAllCategories);
-router.post("/", createCategory);
+router.get("/:id", categoryIdValidation(), validateRequest, getCategory);
+router.post("/", categoryValidationRules(), validateRequest, createCategory);
+router.put(
+  "/:id",
+  categoryIdValidation(),
+  categoryUpdateValidationRules(),
+  validateRequest,
+  updateCategory
+);
+router.delete("/:id", categoryIdValidation(), validateRequest, deleteCategory);
 
 module.exports = router;
