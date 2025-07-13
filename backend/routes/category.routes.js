376891aa9a806734af91730e978d099b4a4976f8
Agustin Loos <loosagustin@gmail.com>
2025-07-13 11:59:16 -0300

    agregar autenticación a las rutas de categoría

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { getAllCategories, createCategory } = require("../controllers/category.controller");
+const { authenticate } = require("../middleware/auth");
 const router = express.Router();
 
+router.use(authenticate);
 router.get("/", getAllCategories);
 router.post("/", createCategory);
 
