ae9e2d705171884b6e99e6a4f9573ec82bfa699b
Agustin Loos <loosagustin@gmail.com>
2025-07-13 16:42:54 -0300

    refactor: ajustar el esquema de transacciones para mejorar la estructura de datos

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -2,23 +2,32 @@ const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema(
   {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      ref: "User",
+      maxlength: [50, "El nombre de usuario no puede exceder 50 caracteres"],
+    },
     amount: {
       type: Number,
       required: true,
     },
     type: {
       type: String,
-      enum: ['income', 'expense'],
+      enum: ["income", "expense"],
       required: true,
+      ref: "TransactionType",
     },
     category: {
       type: String,
+      ref: "Category",
       required: true,
     },
     description: {
       type: String,
       required: false,
-    }
+    },
   },
   {
     timestamps: true,
