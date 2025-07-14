16241633996ccdae0b8fb2eb88224c7bb5e40374
Agustin Loos <loosagustin@gmail.com>
2025-07-14 17:03:26 -0300

    feat: agregar manejo de errores en las funciones del controlador de categorías

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,46 +1,68 @@
 const CategoryService = require("../services/category.service");
 const ApiResponse = require("../utils/ApiResponse");
 
-const getAllCategories = async (req, res) => {
-  const result = await CategoryService.getAllCategories();
-  res
-    .status(200)
-    .json(new ApiResponse(200, result, "Categorías obtenidas exitosamente"));
+const getAllCategories = async (req, res, next) => {
+  try {
+    const result = await CategoryService.getAllCategories();
+    res
+      .status(200)
+      .json(new ApiResponse(200, result, "Categorías obtenidas exitosamente"));
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getCategory = async (req, res) => {
-  const category = await CategoryService.getCategoryById(req.params.id);
-  res.status(200).json(new ApiResponse(200, category, "Categoría encontrada"));
+const getCategory = async (req, res, next) => {
+  try {
+    const category = await CategoryService.getCategoryById(req.params.id);
+    res
+      .status(200)
+      .json(new ApiResponse(200, category, "Categoría encontrada"));
+  } catch (error) {
+    next(error);
+  }
 };
 
-const createCategory = async (req, res) => {
-  const newCategory = await CategoryService.createCategory(req.body);
-  res
-    .status(201)
-    .json(new ApiResponse(201, newCategory, "Categoría creada exitosamente"));
+const createCategory = async (req, res, next) => {
+  try {
+    const newCategory = await CategoryService.createCategory(req.body);
+    res
+      .status(201)
+      .json(new ApiResponse(201, newCategory, "Categoría creada exitosamente"));
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateCategory = async (req, res) => {
-  const updatedCategory = await CategoryService.updateCategory(
-    req.params.id,
-    req.body
-  );
-  res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        updatedCategory,
-        "Categoría actualizada exitosamente"
-      )
+const updateCategory = async (req, res, next) => {
+  try {
+    const updatedCategory = await CategoryService.updateCategory(
+      req.params.id,
+      req.body
     );
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          updatedCategory,
+          "Categoría actualizada exitosamente"
+        )
+      );
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteCategory = async (req, res) => {
-  const result = await CategoryService.deleteCategory(req.params.id);
-  res
-    .status(200)
-    .json(new ApiResponse(200, result, "Categoría eliminada exitosamente"));
+const deleteCategory = async (req, res, next) => {
+  try {
+    const result = await CategoryService.deleteCategory(req.params.id);
+    res
+      .status(200)
+      .json(new ApiResponse(200, result, "Categoría eliminada exitosamente"));
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
