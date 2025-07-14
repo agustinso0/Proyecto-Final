36c627901f84d6ae508581b62fc0b096e94129ab
Agustin Loos <loosagustin@gmail.com>
2025-07-14 17:03:29 -0300

    feat: agregar manejo de errores en las funciones del controlador de transacciones

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -1,101 +1,134 @@
 const TransactionService = require("../services/transaction.service");
 const ApiResponse = require("../utils/ApiResponse");
 
-const getAllTransactions = async (req, res) => {
-  // Extraer filtros de los query parameters
-  const filters = {};
-  
-  if (req.query.category) {
-    filters.category = req.query.category;
-  }
-  
-  if (req.query.startDate) {
-    filters.startDate = req.query.startDate;
-  }
-  
-  if (req.query.endDate) {
-    filters.endDate = req.query.endDate;
-  }
+const getAllTransactions = async (req, res, next) => {
+  try {
+    // Extraer filtros de los query parameters
+    const filters = {};
 
-  const result = await TransactionService.getAllTransactions(req.user._id, filters);
-  res
-    .status(200)
-    .json(new ApiResponse(200, result, "Transacciones obtenidas exitosamente"));
-};
+    if (req.query.category) {
+      filters.category = req.query.category;
+    }
 
-const getTransaction = async (req, res) => {
-  const transaction = await TransactionService.getTransactionById(
-    req.params.id,
-    req.user._id
-  );
-  res
-    .status(200)
-    .json(new ApiResponse(200, transaction, "Transacción encontrada"));
-};
+    if (req.query.startDate) {
+      filters.startDate = req.query.startDate;
+    }
 
-const createTransaction = async (req, res) => {
-  const transactionData = {
-    ...req.body,
-    userId: req.user._id,
-  };
-  const newTransaction = await TransactionService.createTransaction(
-    transactionData
-  );
-  res
-    .status(201)
-    .json(
-      new ApiResponse(201, newTransaction, "Transacción creada exitosamente")
+    if (req.query.endDate) {
+      filters.endDate = req.query.endDate;
+    }
+
+    const result = await TransactionService.getAllTransactions(
+      req.user._id,
+      filters
     );
+    res
+      .status(200)
+      .json(
+        new ApiResponse(200, result, "Transacciones obtenidas exitosamente")
+      );
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateTransaction = async (req, res) => {
-  const updatedTransaction = await TransactionService.updateTransaction(
-    req.params.id,
-    req.body,
-    req.user._id
-  );
-  res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        updatedTransaction,
-        "Transacción actualizada exitosamente"
-      )
+const getTransaction = async (req, res, next) => {
+  try {
+    const transaction = await TransactionService.getTransactionById(
+      req.params.id,
+      req.user._id
     );
+    res
+      .status(200)
+      .json(new ApiResponse(200, transaction, "Transacción encontrada"));
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteTransaction = async (req, res) => {
-  const result = await TransactionService.deleteTransaction(
-    req.params.id,
-    req.user._id
-  );
-  res
-    .status(200)
-    .json(new ApiResponse(200, result, "Transacción eliminada exitosamente"));
-};
-
-const getSummary = async (req, res) => {
-  const summary = await TransactionService.getSummary(req.user._id);
-  res
-    .status(200)
-    .json(new ApiResponse(200, summary, "Resumen generado exitosamente"));
-};
-
-const getTransactionsByCategory = async (req, res) => {
-  const transactions = await TransactionService.getTransactionsByCategory(
-    req.params.categoryId,
-    req.user._id
-  );
-  res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        transactions,
-        "Transacciones por categoría obtenidas exitosamente"
-      )
+const createTransaction = async (req, res, next) => {
+  try {
+    const transactionData = {
+      ...req.body,
+      userId: req.user._id,
+    };
+    const newTransaction = await TransactionService.createTransaction(
+      transactionData
     );
+    res
+      .status(201)
+      .json(
+        new ApiResponse(201, newTransaction, "Transacción creada exitosamente")
+      );
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateTransaction = async (req, res, next) => {
+  try {
+    const updatedTransaction = await TransactionService.updateTransaction(
+      req.params.id,
+      req.body,
+      req.user._id
+    );
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          updatedTransaction,
+          "Transacción actualizada exitosamente"
+        )
+      );
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteTransaction = async (req, res, next) => {
+  try {
+    const result = await TransactionService.deleteTransaction(
+      req.params.id,
+      req.user._id
+    );
+    res
+      .status(200)
+      .json(new ApiResponse(200, result, "Transacción eliminada exitosamente"));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getSummary = async (req, res, next) => {
+  try {
+    const summary = await TransactionService.getSummary(req.user._id);
+    res
+      .status(200)
+      .json(new ApiResponse(200, summary, "Resumen generado exitosamente"));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getTransactionsByCategory = async (req, res, next) => {
+  try {
+    const transactions = await TransactionService.getTransactionsByCategory(
+      req.params.categoryId,
+      req.user._id
+    );
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          transactions,
+          "Transacciones por categoría obtenidas exitosamente"
+        )
+      );
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
